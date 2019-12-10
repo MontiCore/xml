e546f1546fa43e90132934c7c384cb5eb3a1471e
n_jansen <nico.jansen1@rwth-aachen.de>
2019-12-10 12:48:47 +0100

    Add simple test case.

---
+++ src/test/java/xml/parser/XMLParserTest.java
@@ -0,0 +1,28 @@
+package xml.parser;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import org.antlr.v4.runtime.RecognitionException;
+import org.junit.Test;
+
+import xml._ast.ASTXMLDocument;
+import xml._parser.XMLParser;
+
+public class XMLParserTest {
+  
+  @Test
+  public void testBookstore() throws RecognitionException, IOException {
+    Path model = Paths.get("src/test/resources/xml/parser/bookstore.xml");
+    XMLParser parser = new XMLParser();
+    
+    Optional<ASTXMLDocument> jsonDoc = parser.parse(model.toString());
+    assertFalse(parser.hasErrors());
+    assertTrue(jsonDoc.isPresent());
+  }
+  
+}
