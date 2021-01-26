b220eeb5f75af9c3cf4276fd16ab1cbc0f60fc75
Nico Jansen <nico.jansen1@rwth-aachen.de>
2021-01-26 13:59:37 +0100

    Re-enable version 6.7.0-SNAPSHOT.

--- src/test/java/de/monticore/lang/xmllight/prettyprint/XMLLightToJSONTest.java
+++ src/test/java/de/monticore/lang/xmllight/prettyprint/XMLLightToJSONTest.java
@@ -1,39 +1,39 @@
-//package de.monticore.lang.xmllight.prettyprint;
-//
-//import de.monticore.XMLLightCLI;
-//import de.monticore.lang.json._ast.ASTJSONDocument;
-//import de.monticore.lang.json._parser.JSONParser;
-//import de.monticore.lang.xmllight._ast.ASTXMLDocument;
-//import de.monticore.lang.xmllight._parser.XMLLightParser;
-//import org.antlr.v4.runtime.RecognitionException;
-//import org.junit.Test;
-//
-//import java.io.IOException;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.util.Optional;
-//
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-//
-//public class XMLLightToJSONTest {
-//	@Test
-//	public void testBookstore() throws RecognitionException, IOException {
-//		Path model = Paths.get("src/test/resources/xml/prettyprint/bookstore.xml");
-//		XMLLightParser parser = new XMLLightParser();
-//
-//		// parse model
-//		Optional<ASTXMLDocument> xmlDoc = parser.parse(model.toString());
-//		assertFalse(parser.hasErrors());
-//		assertTrue(xmlDoc.isPresent());
-//
-//		// pretty print AST
-//		XMLLightToJSON pp = new XMLLightToJSON();
-//		String printedModel = pp.printXMLDocument(xmlDoc.get());
-//
-//		JSONParser jsonParser = new JSONParser();
-//		Optional<ASTJSONDocument> doc =jsonParser.parse_String(printedModel);
-//		assertTrue(doc.isPresent());
-//		assertFalse(jsonParser.hasErrors());
-//	}
-//}
+package de.monticore.lang.xmllight.prettyprint;
+
+import de.monticore.XMLLightCLI;
+import de.monticore.lang.json._ast.ASTJSONDocument;
+import de.monticore.lang.json._parser.JSONParser;
+import de.monticore.lang.xmllight._ast.ASTXMLDocument;
+import de.monticore.lang.xmllight._parser.XMLLightParser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class XMLLightToJSONTest {
+	@Test
+	public void testBookstore() throws RecognitionException, IOException {
+		Path model = Paths.get("src/test/resources/xml/prettyprint/bookstore.xml");
+		XMLLightParser parser = new XMLLightParser();
+
+		// parse model
+		Optional<ASTXMLDocument> xmlDoc = parser.parse(model.toString());
+		assertFalse(parser.hasErrors());
+		assertTrue(xmlDoc.isPresent());
+
+		// pretty print AST
+		XMLLightToJSON pp = new XMLLightToJSON();
+		String printedModel = pp.printXMLDocument(xmlDoc.get());
+
+		JSONParser jsonParser = new JSONParser();
+		Optional<ASTJSONDocument> doc =jsonParser.parse_String(printedModel);
+		assertTrue(doc.isPresent());
+		assertFalse(jsonParser.hasErrors());
+	}
+}
